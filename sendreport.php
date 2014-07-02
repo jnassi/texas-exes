27351216b92cfa12a9469454cff011ac04b37b29
Jason Nassi <jason.nassi@tidalscale.com>
2014-07-01 20:12:58 -0700

    Refactoring with functions

--- sendreport.php
+++ sendreport.php
@@ -1,20 +1,49 @@
 <?php
 
-$CallSid = "CallSid: " . $_POST['CallSid'];
-$From = "From Number: " . $_POST['From'];
-$CallStatus = $_POST['CallStatus'];
-$Direction = $_POST['Direction'];
-$FromCity = "From City: " . $_POST['FromCity'];
-$FromState = "From State: " . $_POST['FromState'];
-$CallDuration = "Duration: " . round($_POST['CallDuration']/60,2) . " minutes";
-
 require_once('../twilio/Services/Twilio.php');
+require_once('./twilio-config.php');
 
-$fp = fopen('mylog.txt','a');
-fwrite($fp, $CallSid . PHP_EOL . $From . PHP_EOL . $FromCity . PHP_EOL . $FromState . PHP_EOL . $CallDuration . PHP_EOL . '-----' . PHP_EOL);
+$client = new Services_Twilio($sid, $token);
 
-fclose($fp);
+function getConference() {
+	
+// Loop over the list of conferences and echo a property for each one
+foreach ($client->account->conferences as $conference) {
+    echo $conference->status;
+		
+}
 
+
+function callDetail() {
+
+	$CallSid = $_POST['CallSid'];
+	$From = $_POST['From'];
+	$CallStatus = $_POST['CallStatus'];
+	$Direction = $_POST['Direction'];
+	$FromCity = $_POST['FromCity'];
+	$FromState = $_POST['FromState'];
+	$CallDuration = round($_POST['CallDuration']/60,2);
+
+}
+
+function writeToLog() {
+
+	$fp = fopen('mylog.txt','a');
+
+	fwrite(
+		$fp, "CallSid: " . $CallSid . PHP_EOL . 
+		"Caller ID: " . $From . PHP_EOL . 
+		"From City: " . $FromCity . PHP_EOL . 
+		"From State: " . $FromState . PHP_EOL . 
+		"Duration: " . $CallDuration . PHP_EOL . " minutes" 
+		"-----" . PHP_EOL
+	);	
+
+	fclose($fp);
+		
+}
+
+writeToLog();
 
 ?>
 
