6c12ebed9ff193c347be14852d58f67ab126595f
Jason Nassi <jason.nassi@tidalscale.com>
2014-07-01 17:52:42 -0700

    Call details now go to a text log file; next step is to send emails

--- sendreport.php
+++ sendreport.php
@@ -1,5 +1,20 @@
 <?php
 
-file_put_contents('mylog.log',"\r\ntestline",FILE_APPEND);
+$CallSid = "CallSid: " . $_POST['CallSid'];
+$From = "From Number: " . $_POST['From'];
+$CallStatus = $_POST['CallStatus'];
+$Direction = $_POST['Direction'];
+$FromCity = "From City: " . $_POST['FromCity'];
+$FromState = "From State: " . $_POST['FromState'];
+$CallDuration = "Duration: " . round($_POST['CallDuration']/60,2) . " minutes";
+
+require_once('../twilio/Services/Twilio.php');
+
+$fp = fopen('mylog.txt','a');
+fwrite($fp, $CallSid . PHP_EOL . $From . PHP_EOL . $FromCity . PHP_EOL . $FromState . PHP_EOL . $CallDuration . PHP_EOL . '-----' . PHP_EOL);
+
+fclose($fp);
+
 
 ?>
+
